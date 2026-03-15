5defe74d9cb2367579271b7e72bc1cff91faf15e
Your Name <quangkhoa1792013@gmail.com>
2026-03-15 16:05:09 +0700

    Update_15/03/2026_16:05:09,04

--- push.bat
+++ push.bat
@@ -1,4 +1,17 @@
 @echo off
+setlocal
+title GitHub Auto-Push
+echo [1/3] Dang gom file...
 git add .
-git commit -m "Upload"
-git push -u origin main
+
+:: Lay thoi gian hien tai lam commit message
+set msg=Update_%date%_%time%
+echo [2/3] Dang commit: %msg%
+git commit -m "%msg%"
+
+echo [3/3] Dang push len GitHub...
+git push origin main
+
+echo.
+echo === XONG! ===
+timeout /t 3
