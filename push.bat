5ad8b9864fe97299d8310caf466c23ff1c660320
Your Name <quangkhoa1792013@gmail.com>
2026-03-17 21:24:19 +0700

    push

--- push.bat
+++ push.bat
@@ -1,23 +1,19 @@
 @echo off
-echo ==========================================
-echo           DANG PUSH THANG 2 MAT TRAN
-echo ==========================================
-
-:: 1. Commit nhanh
+:: 1. Chuan bi quan luong
 git add .
-set /p msg="Ghi chu: "
+set /p msg="Nhap ghi chu: "
 if "%msg%"=="" set msg="push"
 git commit -m "%msg%"
 
-:: 2. Push ca du an len GitHub
-echo [1/2] GITHUB...
+:: 2. Push tong len GitHub
+echo [*] Dang push GitHub...
 git push origin hugging-face-deploy --force
 
-:: 3. Push "ruột" folder backend lên Hugging Face
-echo [2/2] HUGGING FACE (BACKEND)...
+:: 3. Push thang folder backend len Hugging Face
+echo [*] Dang push Hugging Face...
 git subtree push --prefix backend hf main
 
 echo ==========================================
-echo                 XONG!
+echo             DA PUSH XONG!
 echo ==========================================
 pause
